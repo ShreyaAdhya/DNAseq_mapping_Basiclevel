d16914c5d0b492c7513a0ea2f8e7ed261d77e6ae
ShreyaAdhya <76802799+ShreyaAdhya@users.noreply.github.com>
2021-06-12 21:38:03 +0530

    Update RepeatDNA.java

--- RepeatDNA.java
+++ RepeatDNA.java
@@ -1,27 +1,28 @@
-public class Repeat4
+public class RepeatDNA
 {
 	public static void main(String[] args)
 	{
-	  String seq= "GATCGCAATGATCTAAGTAATGACTAAGTCAAATGC";
+	   String seq= "AATAAATCAATGTCAGTATCTACGTAGCAAT";
       String rep = "AAT";
       int n= seq.length();
-	  int m= rep.length();
-	  int c=0;
+	   int m= rep.length();
+	   int r=0;
       
       for(int i=0; i<n; i++)
       {
-              
+         int c=0;     
          for(int j=0; j<m; j++)
           { 
-               if(seq[i]==rep[j])
+             if(seq.charAt(i)==rep.charAt(j))
                {
                  c++;
                }
                i++;
           } 
+          System.out.println(c);
             if(c==3)
             {
-               n=n+1;               
+               r++;               
             }          
       }     
 
@@ -30,7 +31,7 @@ public class Repeat4
 	   System.out.println("  Your repeat is '"+rep+"'");
 	   
       if(n>=1)
-        System.out.println("  The repeat is present "+n+" time(s)");
+        System.out.println("  The repeat is present "+r+" time(s)");
       else
         System.out.println("  The repeat is absent");        
       System.out.println("  Thank you!");     
